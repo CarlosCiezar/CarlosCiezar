9b019cb724e3f2b56a7714f7efd6b6b924a37175
yffrasquero <yamilffrasquero@gmail.com>
2025-04-07 14:46:10 +0200

    Creando función para ejecutar cada 1ro de cada mes

--- function_app.py
+++ function_app.py
@@ -1,16 +1,25 @@
 import azure.functions as func
 from eurostat import getEurostatData
+import datetime
+import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ADMIN)
 
-@app.function_name(name="eurostat_function") #Nombre interno de la función en Azure
-@app.route(route="eurostat")
-def eurostat(req: func.HttpRequest) -> func.HttpResponse:
+def eurostat_data() -> func.HttpResponse:
     try:
-        response = getEurostatData(req)  # Llamamos a la función externa
+        response = getEurostatData(None)  # Llamamos a la función externa
         if not isinstance(response, func.HttpResponse):  # Validamos el retorno
             return func.HttpResponse("Error interno: respuesta no válida.", status_code=500)
         return response
     except Exception as e:
         return func.HttpResponse(f"Error en la función: {str(e)}", status_code=500)
+    
+
+# --- TIMER Trigger (se ejecuta el día 1 de cada mes) ---
+@app.function_name(name="eurostat_monthly_trigger")
+@app.schedule(schedule="0 0 0 1 * *", arg_name="timer", run_on_startup=False, use_monitor=True)
+def eurostat_monthly_trigger(timer: func.TimerRequest) -> None:
+    logging.info("Ejecutando función mensual desde el Timer Trigger...")
+    eurostat_data()
+
     
